cebf62af4cb99e161c00d1ff049123539954cc75
karthikeya <122973946+karthikeya9900@users.noreply.github.com>
2025-01-08 22:38:50 +0530

    added seperte file for exegution of the sprint program

---
+++ exugution.js
@@ -0,0 +1,29 @@
+export const getOperation = (program, currentIndex) => {
+  const operations = {
+    0: ["copy", 3],
+    1: ["add", 4],
+    2: ["sub", 4],
+    3: ["jump", 2],
+    4: ["jumpIfEqual", 4],
+    5: ["jumpIfLessThan", 4],
+    6: ["put", 3],
+    7: ["mul", 4],
+    8: ["div", 1],
+  };
+  return operations[program[currentIndex]];
+};
+
+export const updateProgram = (program, currentIndex, value) =>
+  (program[currentIndex] = value);
+
+const performOperation = (program, currentIndex) => {
+  const operation = getOperation(program, currentIndex);
+  console.log(operation);
+  return currentIndex + operation[1];
+};
+
+export const exeguteProgram = (program) => {
+  console.log(program);
+
+  return "hello";
+};
